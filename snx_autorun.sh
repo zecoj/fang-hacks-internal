cef215c6d197be7c15cd72457378dcaecc4babe5
zecoj <b@zecoj.com>
2018-05-15 00:03:29 +1000

    enforce md5sum check on files.tar

--- snx_autorun.sh
+++ snx_autorun.sh
@@ -8,11 +8,20 @@ if [ -e /media/${MDEV}/config.txt ] ; then
 	sed -i -e 's/ssid=".*"$/ssid="'${SSID}'"/g' /etc/wpa_supplicant.conf
 	sed -i -e 's/psk=".*"$/psk="'${PSK}'"/g' /etc/wpa_supplicant.conf
 	echo ${TZ} > /etc/TZ
+	if [ -e /etc/rtsp/OK ] ; then
+		reboot
+	fi
 fi
 
-if [ ! -e /etc/rtsp ] ; then
-	sed -i '/iSC3S/d' /etc/init.d/rcS
+MYSUM=`md5sum /media/${MDEV}/files.tar|cut -d " " -f1`
 
+if [ "${MYSUM}" -ne "${MD5SUM}" ] ; then
+	return
+else
+	continue
+fi
+
+if [ ! -e /etc/rtsp/OK ] ; then
 	kill -9 `pidof iCamera`
 	kill -9 `pidof test_UP`
 	kill -9 `pidof iSC3S`
@@ -24,8 +33,12 @@ if [ ! -e /etc/rtsp ] ; then
 	tar xf /media/${MDEV}/files.tar 
 	cp /media/${MDEV}/config.txt .
 
+	sed -i '/iSC3S/d' /etc/init.d/rcS
+
 	sed -i '/rtsp/d' /etc/init.d/rc.local
 	echo "/etc/rtsp/my.rc.local & " >> /etc/init.d/rc.local
+
+	touch /etc/rtsp/OK
 
 	sync && reboot
 fi
